30670c2b2b06d25a53df144d549495fef12f9e5a
jaeshim <sjm767@naver.com>
2025-03-08 09:25:18 +0900

    테스트 코드 추가

--- java.codingtest.study/src/test/java/com/jaeshim/java/codingtest/study/ApplicationTests.java
+++ java.codingtest.study/src/test/java/com/jaeshim/java/codingtest/study/ApplicationTests.java
@@ -1,13 +1,12 @@
 package com.jaeshim.java.codingtest.study;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testSample() {
+		System.out.println("테스트");
 	}
 
 }
