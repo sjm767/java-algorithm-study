90e6566d00e0b183c524e5efb8009e6c937596f0
jaeshim <sjm767@naver.com>
2025-03-08 09:19:29 +0900

    20250308

---
+++ java.codingtest.study/src/main/java/com/jaeshim/java/codingtest/study/Application.java
@@ -0,0 +1,15 @@
+package com.jaeshim.java.codingtest.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
